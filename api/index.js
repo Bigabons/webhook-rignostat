137926a03a7dba20795d317e88a57698df10aa97
Bigabons <165175202+Bigabons@users.noreply.github.com>
2024-11-15 15:28:06 +0100

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,9 +1,7 @@
-const handler = (req, res) => {
+export default function handler(req, res) {
   if (req.method === 'POST') {
     res.json({ message: 'Hello from API!' });
   } else {
     res.status(405).json({ error: 'Method not allowed' });
   }
-};
-
-module.exports = handler;
+}
